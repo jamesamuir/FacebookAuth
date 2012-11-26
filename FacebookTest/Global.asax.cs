1d9d040b8a06e5bfc3ea221734f48ef05558e284
unknown <US084134@US194NBO5SYZZL1.us.tycoelectronics.com>
2012-11-25 22:26:42 -0500

    added route to ignore favicon

--- FacebookTest/Global.asax.cs
+++ FacebookTest/Global.asax.cs
@@ -20,6 +20,7 @@ namespace FacebookTest
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
 
             routes.MapRoute(
                 "Default", // Route name
